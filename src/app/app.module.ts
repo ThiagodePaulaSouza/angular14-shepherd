ef24af7ff21d6bec94e9ace4054686eff46fd7e5
Thiago de Paula Souza <thiagopaulasouza@gmail.com>
2022-12-16 09:37:17 -0300

    css shepherd editable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ShepherdComponent } from './shepherd/shepherd.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent,ShepherdComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
